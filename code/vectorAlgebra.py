ae90314c6697e8d40f037ec94c5e8571de861e6d
bartbal <bartvannetburg@hotmail.com>
2019-07-09 16:45:50 +0200

    created vectorAlgebra class

---
+++ code/vectorAlgebra.py
@@ -0,0 +1,40 @@
+import math
+
+class VectorAlgebra():
+    
+    # multiply a one-dimensional vector by a number
+    def vectorNumMultiply(vector, num): 
+        result = list(vector)
+
+        for index in range(len(result)):
+            result[index] *= num
+
+        return result
+
+    # calculate the dot product of two one-dimensional vectors. <a, b>
+    def dotProduct(a, b):
+        assert len(a) == len(b), "a and b are not the same length"
+
+        result = 0
+
+        for index in range(len(a)):
+            result += (a[index] * b[index])
+
+        return result
+    
+    # calculate the unit vector of a one-dimensional vector.
+    def unitVector(a):
+        length = VectorAlgebra.vectorLength(a)
+        return [a[0]/length, a[1]/length]
+
+    # calculate the linear length of a one-dimensional vector. |a|
+    def vectorLength(a):
+        return  math.sqrt( a[0]**2 + a[1]**2 )
+
+    # calculate the projection of a on b. Both have to be a one-dimensional vector.
+    def projection(a, b):
+        return VectorAlgebra.vectorNumMultiply(b, (VectorAlgebra.dotProduct(a, b) / VectorAlgebra.vectorLength(b)**2))
+
+    # calculates the length of the projection of a on b
+    def projectionLength(a, b):
+        return (VectorAlgebra.dotProduct(a, b) / VectorAlgebra.vectorLength(b))
